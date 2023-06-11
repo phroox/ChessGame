7afacfb3ec9218c2af6509d1f36e6be71e5ba06e
Philippe Simões <ph_simoes@id.uff.br>
2023-06-11 14:12:13 -0300

    create def __init__ with moves in gamestate Class

--- chess/ChessEngine.py
+++ chess/ChessEngine.py
@@ -6,10 +6,12 @@ It will keep move log.
 
 class GameState():
     def __init__(self):
-        #Board is an 8x8 2d list, each element in list has 2 characters.
-        #The first character represtents the color of the piece: 'b' or 'w'.
-        #The second character represtents the type of the piece: 'R', 'N', 'B', 'Q', 'K' or 'p'.
-        #"--" represents an empty space with no piece.
+        """
+        Board is an 8x8 2d list, each element in list has 2 characters.
+        The first character represents the color of the piece: 'b' or 'w'.
+        The second character represents the type of the piece: 'R', 'N', 'B', 'Q', 'K' or 'p'.
+        "--" represents an empty space with no piece.
+        """
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
@@ -19,5 +21,19 @@ class GameState():
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
-        self.whiteToMove = True
-        self.moveLog = []
+        self.moveFunctions = {"p": self.getPawnMoves, "R": self.getRookMoves, "N": self.getKnightMoves,
+                              "B": self.getBishopMoves, "Q": self.getQueenMoves, "K": self.getKingMoves}
+        self.white_to_move = True
+        self.move_log = []
+        self.white_king_location = (7, 4)
+        self.black_king_location = (0, 4)
+        self.checkmate = False
+        self.stalemate = False
+        self.in_check = False
+        self.pins = []
+        self.checks = []
+        self.enpassant_possible = ()  # coordinates for the square where en-passant capture is possible
+        self.enpassant_possible_log = [self.enpassant_possible]
+        self.current_castling_rights = CastleRights(True, True, True, True)
+        self.castle_rights_log = [CastleRights(self.current_castling_rights.wks, self.current_castling_rights.bks,
+                                               self.current_castling_rights.wqs, self.current_castling_rights.bqs)]
